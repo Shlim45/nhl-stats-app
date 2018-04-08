e3e61181b5a7f7cb4de5dd748abb21d3e69b32d2
Shlim45 <jhawranko@gmail.com>
2018-04-08 19:55:34 -0400

    Add sorting PlayerStats ascending, button to view skaters/goalies

--- containers/PlayerStats.js
+++ containers/PlayerStats.js
@@ -9,15 +9,32 @@ class PlayerStats extends React.Component {
 
         this.state = {
             players,
+            skaters: true,
+            sortedBy: 'points',
+            asc: false,
         };
     }
 
     handleSort = e => {
         //TODO ascending/descending
         const sortBy = e.target.id;
-        const { players } = this.state;
-        const newSorting = sortPlayers(players, sortBy, false);
-        this.setState({ players });
+        const { players, sortedBy, asc } = this.state;
+
+        // if sorting by same stat, toggle ascending
+        const newAsc = sortedBy === sortBy ? !asc : false;
+
+        const newSorting = sortPlayers(players, sortBy, newAsc);
+
+        this.setState({ players: newSorting, sortedBy: sortBy, asc: newAsc });
+    };
+
+    togglePlayers = e => {
+        this.setState({ skaters: !this.state.skaters });
+        if (!this.state.skaters) {
+            e.target.textContent = 'View Goalies';
+        } else {
+            e.target.textContent = 'View Skaters';
+        }
     };
 
     render() {
@@ -35,95 +52,120 @@ class PlayerStats extends React.Component {
 
         // TODO: separation of skaters and goalies
 
-        const { players } = this.state;
+        let { players } = this.state;
+
+        if (this.state.skaters) {
+            players = players.filter(p => p.primaryPosition.code !== 'G');
+        } else {
+            players = players.filter(p => p.primaryPosition.code === 'G');
+        }
 
         return (
-            <ul className="player-list list-group mb-5">
-                <li
-                    key={'player-list-heading'}
-                    className="list-group-item active d-flex justify-content-between align-items-center"
-                >
-                    <span className="badge badge-secondary badge-pill playerstats-rank">Rank</span>
-                    <button
-                        className="playerstats-fullname"
-                        id="lastName"
-                        onClick={this.handleSort}
+            <div className="container">
+                <button className="btn btn-sm btn-secondary mb-5" onClick={this.togglePlayers}>
+                    View Goalies
+                </button>
+                <ul className="player-list list-group mb-5">
+                    <li
+                        key={'player-list-heading'}
+                        className="list-group-item active d-flex justify-content-between align-items-center"
                     >
-                        Player Name
-                    </button>
-                    <button className="playerstats-season" id="season" onClick={this.handleSort}>
-                        Season
-                    </button>
-                    <button className="playerstats-stat" id="games" onClick={this.handleSort}>
-                        GP
-                    </button>
-                    <button className="playerstats-stat" id="goals" onClick={this.handleSort}>
-                        G
-                    </button>
-                    <button className="playerstats-stat" id="assists" onClick={this.handleSort}>
-                        A
-                    </button>
-                    <button className="playerstats-stat desc" id="points" onClick={this.handleSort}>
-                        Pts
-                    </button>
-                    <button className="playerstats-stat" id="plusMinus" onClick={this.handleSort}>
-                        +/-
-                    </button>
-                </li>
-                {players.map((player, i) => {
-                    const { stat } = player.stats[0];
-                    return (
-                        <li
-                            key={player.jerseyNumber}
-                            className="list-group-item d-flex justify-content-between align-items-center"
+                        <span className="badge badge-secondary badge-pill playerstats-rank">
+                            Rank
+                        </span>
+                        <button
+                            className="playerstats-fullname"
+                            id="lastName"
+                            onClick={this.handleSort}
                         >
-                            <span className="badge badge-secondary badge-pill playerstats-rank">
-                                {i + 1}
-                            </span>
-                            <span className="playerstats-fullname">{player.fullName}</span>
-                            <span className="playerstats-season">2017-18</span>
-                            <span className="playerstats-stat">{stat.games}</span>
-                            <span className="playerstats-stat">{stat.goals}</span>
-                            <span className="playerstats-stat">{stat.assists}</span>
-                            <span className="playerstats-stat">{stat.points}</span>
-                            <span className="playerstats-stat">{stat.plusMinus}</span>
-                        </li>
-                    );
-                })}
-                <style jsx>{`
-                    .player-list {
-                        max-width: 850px;
-                        min-width: 500px;
-                    }
+                            Player Name
+                        </button>
+                        <button
+                            className="playerstats-season"
+                            id="season"
+                            onClick={this.handleSort}
+                        >
+                            Season
+                        </button>
+                        <button className="playerstats-stat" id="games" onClick={this.handleSort}>
+                            GP
+                        </button>
+                        <button className="playerstats-stat" id="goals" onClick={this.handleSort}>
+                            G
+                        </button>
+                        <button className="playerstats-stat" id="assists" onClick={this.handleSort}>
+                            A
+                        </button>
+                        <button
+                            className="playerstats-stat desc"
+                            id="points"
+                            onClick={this.handleSort}
+                        >
+                            Pts
+                        </button>
+                        <button
+                            className="playerstats-stat"
+                            id="plusMinus"
+                            onClick={this.handleSort}
+                        >
+                            +/-
+                        </button>
+                    </li>
+                    {players.map((player, i) => {
+                        const { stat } = player.stats[0];
+                        return (
+                            <li
+                                key={player.jerseyNumber}
+                                className="list-group-item d-flex justify-content-between align-items-center"
+                            >
+                                <span className="badge badge-secondary badge-pill playerstats-rank">
+                                    {i + 1}
+                                </span>
+                                <span className="playerstats-fullname">{player.fullName}</span>
+                                <span className="playerstats-season">2017-18</span>
+                                <span className="playerstats-stat">{stat.games}</span>
+                                <span className="playerstats-stat">{stat.goals}</span>
+                                <span className="playerstats-stat">{stat.assists}</span>
+                                <span className="playerstats-stat">{stat.points}</span>
+                                <span className="playerstats-stat">{stat.plusMinus}</span>
+                            </li>
+                        );
+                    })}
+                    <style jsx>{`
+                        .player-list {
+                            max-width: 850px;
+                            min-width: 500px;
+                        }
 
-                    .player-list button {
-                        background-color: inherit;
-                        border: none;
-                        font: inherit;
-                        color: inherit;
-                    }
+                        .player-list button {
+                            background-color: inherit;
+                            border: none;
+                            font: inherit;
+                            color: inherit;
+                        }
 
-                    .player-list button:hover {
-                        cursor: pointer;
-                    }
+                        .player-list button:hover {
+                            cursor: pointer;
+                        }
 
-                    .playerstats-rank {
-                        width: 2.5rem;
-                    }
+                        .playerstats-rank {
+                            width: 2.5rem;
+                        }
 
-                    .playerstats-fullname {
-                        width: 10rem;
-                    }
+                        .playerstats-fullname {
+                            width: 10rem;
+                        }
 
-                    .playerstats-season {
-                        width: 4rem;
-                    }
+                        .playerstats-season {
+                            width: 4rem;
+                        }
 
-                    .playerstats-stat {
-                        width: 2rem;
-                    }
-                `}</style>
-            </ul>
+                        .playerstats-stat {
+                            width: 2rem;
+                        }
+                    `}</style>
+                </ul>
+            </div>
         );
     }
 }
--- handlers/sort.js
+++ handlers/sort.js
@@ -1,41 +1,43 @@
 export function sortPlayers(players, sortBy, ascend) {
+    let newSorting = [...players];
+    console.log({ newSorting });
     if (ascend) {
         switch (sortBy) {
             case 'lastName':
-                return players.sort((a, b) => a.lastName > b.lastName);
+                return newSorting.sort((a, b) => a.lastName > b.lastName);
             case 'games':
-                return players.sort((a, b) => a.stats[0].stat.games - b.stats[0].stat.games);
+                return newSorting.sort((a, b) => a.stats[0].stat.games - b.stats[0].stat.games);
             case 'goals':
-                return players.sort((a, b) => a.stats[0].stat.goals - b.stats[0].stat.goals);
+                return newSorting.sort((a, b) => a.stats[0].stat.goals - b.stats[0].stat.goals);
             case 'assists':
-                return players.sort((a, b) => a.stats[0].stat.assists - b.stats[0].stat.assists);
+                return newSorting.sort((a, b) => a.stats[0].stat.assists - b.stats[0].stat.assists);
             case 'points':
-                return players.sort((a, b) => a.stats[0].stat.points - b.stats[0].stat.points);
+                return newSorting.sort((a, b) => a.stats[0].stat.points - b.stats[0].stat.points);
             case 'plusMinus':
-                return players.sort(
+                return newSorting.sort(
                     (a, b) => a.stats[0].stat.plusMinus - b.stats[0].stat.plusMinus
                 );
             default:
-                return players;
+                return newSorting;
         }
     } else {
         switch (sortBy) {
             case 'lastName':
-                return players.sort((a, b) => b.lastName < a.lastName);
+                return newSorting.sort((a, b) => b.lastName < a.lastName);
             case 'games':
-                return players.sort((a, b) => b.stats[0].stat.games - a.stats[0].stat.games);
+                return newSorting.sort((a, b) => b.stats[0].stat.games - a.stats[0].stat.games);
             case 'goals':
-                return players.sort((a, b) => b.stats[0].stat.goals - a.stats[0].stat.goals);
+                return newSorting.sort((a, b) => b.stats[0].stat.goals - a.stats[0].stat.goals);
             case 'assists':
-                return players.sort((a, b) => b.stats[0].stat.assists - a.stats[0].stat.assists);
+                return newSorting.sort((a, b) => b.stats[0].stat.assists - a.stats[0].stat.assists);
             case 'points':
-                return players.sort((a, b) => b.stats[0].stat.points - a.stats[0].stat.points);
+                return newSorting.sort((a, b) => b.stats[0].stat.points - a.stats[0].stat.points);
             case 'plusMinus':
-                return players.sort(
+                return newSorting.sort(
                     (a, b) => b.stats[0].stat.plusMinus - a.stats[0].stat.plusMinus
                 );
             default:
-                return players;
+                return newSorting;
         }
     }
 }
