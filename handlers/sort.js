1cde678514a0629700e140c5d749cccceb26b0e4
Shlim45 <jhawranko@gmail.com>
2018-04-08 20:08:13 -0400

    Refactor sortPlayers and fix sorting by player name

--- handlers/sort.js
+++ handlers/sort.js
@@ -1,43 +1,17 @@
 export function sortPlayers(players, sortBy, ascend) {
     let newSorting = [...players];
-    console.log({ newSorting });
+
     if (ascend) {
-        switch (sortBy) {
-            case 'lastName':
-                return newSorting.sort((a, b) => a.lastName > b.lastName);
-            case 'games':
-                return newSorting.sort((a, b) => a.stats[0].stat.games - b.stats[0].stat.games);
-            case 'goals':
-                return newSorting.sort((a, b) => a.stats[0].stat.goals - b.stats[0].stat.goals);
-            case 'assists':
-                return newSorting.sort((a, b) => a.stats[0].stat.assists - b.stats[0].stat.assists);
-            case 'points':
-                return newSorting.sort((a, b) => a.stats[0].stat.points - b.stats[0].stat.points);
-            case 'plusMinus':
-                return newSorting.sort(
-                    (a, b) => a.stats[0].stat.plusMinus - b.stats[0].stat.plusMinus
-                );
-            default:
-                return newSorting;
+        if (sortBy === 'lastName') {
+            return newSorting.sort((a, b) => a.lastName.toLowerCase() < b.lastName.toLowerCase() ? -1 : 1);
+        } else {
+            return newSorting.sort((a, b) => a.stats[0].stat[sortBy] - b.stats[0].stat[sortBy]);
         }
     } else {
-        switch (sortBy) {
-            case 'lastName':
-                return newSorting.sort((a, b) => b.lastName < a.lastName);
-            case 'games':
-                return newSorting.sort((a, b) => b.stats[0].stat.games - a.stats[0].stat.games);
-            case 'goals':
-                return newSorting.sort((a, b) => b.stats[0].stat.goals - a.stats[0].stat.goals);
-            case 'assists':
-                return newSorting.sort((a, b) => b.stats[0].stat.assists - a.stats[0].stat.assists);
-            case 'points':
-                return newSorting.sort((a, b) => b.stats[0].stat.points - a.stats[0].stat.points);
-            case 'plusMinus':
-                return newSorting.sort(
-                    (a, b) => b.stats[0].stat.plusMinus - a.stats[0].stat.plusMinus
-                );
-            default:
-                return newSorting;
+        if (sortBy === 'lastName') {
+            return newSorting.sort((a, b) => a.lastName.toLowerCase() < b.lastName.toLowerCase() ? 1 : -1);
+        } else {
+            return newSorting.sort((a, b) => b.stats[0].stat[sortBy] - a.stats[0].stat[sortBy]);
         }
     }
 }
